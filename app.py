d07426f2e9cb2cccdb785c60797361ee5ae0ffcf
suriya1410 <79642202+suriya1410@users.noreply.github.com>
2021-07-03 09:14:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'database-my.caomyyms75ok.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'suriya'
+app.config['MYSQL_PASSWORD'] = 'suriya123'
+app.config['MYSQL_DB'] ='regform'
+
+mysql = MySQL(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        name = details['name']
+        age = details['age']
+        email = details['email']
+        mobile = details['mobile']
+        location = details['location']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO userdata(name, age, email, mobile, location) VALUES (%s, %s, %s, %s)", (name, age, email, mobile, location))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+
+@app.route('/users')
+def users():
+    cur =mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM userdata")
+    if resultValue > 0:
+        usersDetails = cur.fetchall()
+
+        return render_template('users.html',usersDetails=usersDetails)
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0",port=80)
