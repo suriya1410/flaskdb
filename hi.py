064a40f07964d80d2ad99a879ef9c7ec4986ac68
suriya1410 <79642202+suriya1410@users.noreply.github.com>
2021-07-28 10:46:42 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World suriya'
+  return 'Hello World Suriya'
 
-if _name_ == '_main_':
-   app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=80)
