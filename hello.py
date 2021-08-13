4978fe30e2a6ebde5a0d298ba4bd939863224f19
suriya1410 <79642202+suriya1410@users.noreply.github.com>
2021-08-13 11:34:52 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,7 +9,6 @@ db = mysql.connect(
 cursor = db.cursor()
 cursor.execute("CREATE DATABASE IF NOT EXISTS regform")
 cursor.commit()
-cursor.close()
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'database-my.caomyyms75ok.us-east-1.rds.amazonaws.com'
